3f78bdebcc288a5477a3a5ed39f30b252dac75d9
Yogesh <yogiupmanyu@gmail.com>
2021-01-19 15:15:37 +0530

    currency-conversion-service

---
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,13 @@
+package com.example.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="currency-exchange-service",url="localhost:8000")//remove URL attribute when using naming server, then it will provide these details
+public interface CurrencyExchangeServiceProxy {
+
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversionBean retrieveExchangeValue
+		(@PathVariable("from") String from, @PathVariable("to") String to);
+}
